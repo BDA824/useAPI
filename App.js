e289a86398e17212aed145db7fb23987d25e1960
BDA824 <ferneyavila73@gmail.com>
2024-08-24 15:34:28 -0500

    Primer commit

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const useAPI = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/broker/api/v1/managers/')
+        console.log(response.data)
+        setData(response.data)
+        setLoading(false)
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+
+    useAPI()
+  }, []
+  )
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <Text>Nombre: {item.name}</Text>}
+      />
     </View>
   );
 }
@@ -14,7 +44,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
